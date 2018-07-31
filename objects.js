6633db2f37349f14d0bb893c3d8000f1247f75f8
Alena <i.am.am.629@gmail.com>
2018-07-31 22:55:28 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,8 @@ var playlist = new Object({ artist: 'song' });
 function updatePlaylist(list, name, title){
   return Objsect.assign({}, list, { [name]: title })
 }
+
+function removeFromPlaylist(list,name){
+  delete list.name;
+  return list
+}
